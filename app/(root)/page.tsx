574bfc72097d851a301b3ae6d22bd182fca24ab8
Anthony Ogugua <loysiustony@hotmail.com.hk>
2024-05-12 05:24:29 +0100

    singin integration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  const loggedIn = { firstName: 'Anthony'};
 }
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome back!"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Here's what's happening with your account today."
+          />
+
+          <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1} 
+          totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  )
+
 
 export default Home

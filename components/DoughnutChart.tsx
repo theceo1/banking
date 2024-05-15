b1b5e9ac7a84f24c556b1b9db3a182f84a0646bc
Anthony Ogugua <loysiustony@hotmail.com.hk>
2024-05-15 02:06:34 +0100

    Left Sidebar/Menu

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -1,39 +1,41 @@
-"use client";
-
+'use client';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+interface DoughnutChartProps {
+  accounts: {
+    name: string;
+    balance: number;
+  }[];
+}
 
-
-const DoughnutChart = ({ accounts }): DoughnutChartProps => {
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
   const data = {
     datasets: [
       {
         label: 'Banks',
-        data: accounts.map(accounts => accounts.balance),
-        backgroundColor: ['#0747b6', '#2265d8', '#2f91fa']
+        data: accounts.map((account) => account.balance),
+        backgroundColor: ['#0747b6','#2265d8', '#2f91fa']
       }
     ],
-    labels: accounts.map(accounts => accounts.balance),
-    // accounts: accounts
+    labels: accounts.map((account) => account.name)
   };
 
   return (
     <Doughnut
-    data={data}
-    accounts={accounts} 
-    options={{
-        cutout: '60',
+      data={data}
+      options={{
+        cutout: '75%',
         plugins: {
           legend: {
             display: false
           }
         }
-    }}
+      }}
     />
   );
 };
 
-export default DoughnutChart
+export default DoughnutChart;

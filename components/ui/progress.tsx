4990f2ea5559b01c772ee5c5ffe69ae79bcd2328
Anthony Ogugua <loysiustony@hotmail.com.hk>
2024-06-06 17:32:23 +0100

    fixed error

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -5,10 +5,16 @@ import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
 
+type ProgressProps = {
+  value: number;
+  className?: string;
+  indicatorClassName?: string;
+} & React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>;
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ indicatorClassName, className, value, ...props }, ref) => (
+  ProgressProps
+>(({ className, indicatorClassName, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -18,7 +24,10 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className={cn('h-full w-full flex-1 bg-primary transition-all', indicatorClassName)}
+      className={cn(
+        "h-full w-full flex-1 bg-primary transition-all",
+        indicatorClassName
+      )}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
